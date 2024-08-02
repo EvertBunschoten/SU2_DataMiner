c30304efda19ef135ff0a316a716f5a872e78051
EvertBunschoten <evertbun@xs4all.nl>
2024-08-02 19:47:22 +0200

    Updated documentation

--- Common/Config_base.py
+++ Common/Config_base.py
@@ -1,8 +1,8 @@
 ###############################################################################################
-#       #      _____ __  _____      ____        __        __  ____                   #        #  
-#       #     / ___// / / /__ \    / __ \____ _/ /_____ _/  |/  (_)___  ___  _____   #        #  
-#       #     \__ \/ / / /__/ /   / / / / __ `/ __/ __ `/ /|_/ / / __ \/ _ \/ ___/   #        #      
-#       #    ___/ / /_/ // __/   / /_/ / /_/ / /_/ /_/ / /  / / / / / /  __/ /       #        #  
+#       #      _____ __  _____      ____        __        __  ____                   #        #
+#       #     / ___// / / /__ \    / __ \____ _/ /_____ _/  |/  (_)___  ___  _____   #        #
+#       #     \__ \/ / / /__/ /   / / / / __ `/ __/ __ `/ /|_/ / / __ \/ _ \/ ___/   #        #
+#       #    ___/ / /_/ // __/   / /_/ / /_/ / /_/ /_/ / /  / / / / / /  __/ /       #        #
 #       #   /____/\____//____/  /_____/\__,_/\__/\__,_/_/  /_/_/_/ /_/\___/_/        #        #
 #       #                                                                            #        #
 ###############################################################################################
@@ -17,8 +17,8 @@
 #                                                                                             |
 #                                                                                             |
 # Description:                                                                                |
-#  Base class for DataMiner configuration                                                     |                                                               
-#                                                                                             |  
+#  Base class for DataMiner configuration                                                     |
+#                                                                                             |
 # Version: 1.0.0                                                                              |
 #                                                                                             |
 #=============================================================================================#
@@ -27,6 +27,12 @@ import pyfiglet
 from Properties import DefaultProperties 
 
 class Config:
+    """!
+    @brief Base class for DataMiner configurations.
+
+
+    """
+    
     __banner_header = "SU2 DataMiner"
     _output_dir:str = "./"
     _config_name:str = "config"
@@ -36,20 +42,30 @@ class Config:
     __test_fraction:float = DefaultProperties.test_fraction
     
     def __init__(self):
+        """!
+        @brief Class constructor
+
+        """
         return 
     
     def PrintBanner(self):
+        """!
+        @brief Print information about the DataMiner configuration that describe the fluid data set and manifold configuration settings.
+
+        """
         customfig = pyfiglet.Figlet(font="slant")
         print(customfig.renderText(self.__banner_header))
         return 
     
     def SetOutputDir(self, output_dir:str):
-        """
-        Define the fluid data output directory. This directory is set as the default storage directory for all storage processes in the EntropicAI workflow.
+        """!
+        @brief Define the fluid data output directory. This directory is set as the default storage directory for all storage processes in the DataMiner workflow.
 
-        :param output_dir: storage directory.
-        :raise: Exception: If the specified directory does not exist.
+        Parameters : 
+            @param output_dir : str => storage directory.
 
+        Exceptions :
+            @exception Exception if specified directory does not exist on the current hardware.
         """
         if not os.path.isdir(output_dir):
             raise Exception("Invalid output data directory")
@@ -58,12 +74,11 @@ class Config:
         return 
     
     def GetOutputDir(self):
-        """
-        Get the current EntropicAI configuration fluid storage directory.
+        """!
+        @brief Get the current DataMiner configuration fluid storage directory.
 
-        :raises: Exception: if the storage directory in the current `EntropicAIConfig` class is not present on the current hardware.
-        :return: Flamelet data storage directory.
-        :rtype: str
+        Exceptions:
+            @exception Exception if the storage directory in the current configuration class is not present on the current hardware.
 
         """
         if not os.path.isdir(self._output_dir):
@@ -72,28 +87,44 @@ class Config:
             return self._output_dir
     
     def SetConcatenationFileHeader(self, header:str=DefaultProperties.output_file_header):
-        """
-        Define the file name header for the collection of fluid data.
+        """!
+        @brief Define the file name header for the collection of fluid data.
 
-        :param header: file name header.
-        :type header: str
+        Parameters : 
+            @param header : str = DefaultProperties.output_file_header => file name header.
+
         """
         self.__concatenated_file_header = header 
         return 
     
     def GetConcatenationFileHeader(self):
-        """
-        Get the file name header for the fluid collection file.
+        """!
+        @brief Get the file name header for the fluid collection file.
 
-        :return: fluid collection file header.
-        :rtype: str
+        Returns :
+            @return fluid data collection file header.
         """
         return self.__concatenated_file_header
     
     def GetConfigName(self):
+        """!
+        @brief [Function's description]
+
+        Parameters : 
+            @param self => [description]
+
+        """
         return self._config_name 
     
     def SetTrainFraction(self, input:float=DefaultProperties.train_fraction):
+        """!
+        @brief [Function's description]
+
+        Parameters : 
+            @param self => [description]
+            @param input : float = DefaultProperties.train_fraction => [description]
+
+        """
         """
         Define the fraction of fluid data used for training multi-layer perceptrons.
 
@@ -107,6 +138,14 @@ class Config:
         return 
     
     def SetTestFraction(self, input:float=DefaultProperties.test_fraction):
+        """!
+        @brief [Function's description]
+
+        Parameters : 
+            @param self => [description]
+            @param input : float = DefaultProperties.test_fraction => [description]
+
+        """
         """
         Define the fraction of fluid data separate from the training data used for determining accuracy after training.
 
@@ -120,6 +159,13 @@ class Config:
         return 
     
     def GetTrainFraction(self):
+        """!
+        @brief [Function's description]
+
+        Parameters : 
+            @param self => [description]
+
+        """
         """
         Get fluid data fraction used for multi-layer perceptron training.
 
@@ -129,6 +175,13 @@ class Config:
         return self.__train_fraction
     
     def GetTestFraction(self):
+        """!
+        @brief [Function's description]
+
+        Parameters : 
+            @param self => [description]
+
+        """
         """
         Get fluid data fraction used for determining accuracy after training.
 
