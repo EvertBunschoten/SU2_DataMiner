a08ad80742ea09ea85a945aae0e44d944e6d3408
EvertBunschoten <evertbun@xs4all.nl>
2024-08-08 15:11:13 +0200

    Added regression test for physics-informed learning

--- RegressionTests/run_regression.py
+++ RegressionTests/run_regression.py
@@ -10,7 +10,7 @@ def main():
     fluid_air.config_file = "config_air.cfg"
     fluid_air.exec_command = "./generate_fluid_data.py"
     fluid_air.reference_files = ["MLP_data_test_ref.csv"]
-    fluid_air.test_files = ["MLP_data_test.csv"]
+    fluid_air.test_files = ["fluid_data_test.csv"]
     test_list.append(fluid_air)
 
     fluid_MM = TestCase("Fluid_MM")
@@ -18,7 +18,7 @@ def main():
     fluid_MM.config_file = "config_MM.cfg"
     fluid_MM.exec_command = "./generate_fluid_data.py"
     fluid_MM.reference_files = ["MLP_data_test_ref.csv"]
-    fluid_MM.test_files = ["MLP_data_test.csv"]
+    fluid_MM.test_files = ["fluid_data_test.csv"]
     test_list.append(fluid_MM)
 
     training_MM_direct = TestCase("Training_MM_Direct")
@@ -29,6 +29,14 @@ def main():
     training_MM_direct.test_files = ["Model_0/TrainingHistory.csv"]
     test_list.append(training_MM_direct)
 
+    training_MM_PINN = TestCase("Training_MM_PhysicsInformed")
+    training_MM_PINN.config_dir = "FluidTraining/MM_PINN/"
+    training_MM_PINN.config_file = "config_MM.cfg"
+    training_MM_PINN.exec_command = "./train_MLP.py"
+    training_MM_PINN.reference_files = ["SU2_MLP_ref.mlp"]
+    training_MM_PINN.test_files = ["Model_0/SU2_MLP.mlp"]
+
+    test_list.append(training_MM_PINN)
     pass_list = [test.run_test() for test in test_list]
 
     # Tests summary
