de071cdf5788c7f0b52ef0719342d48268098aca
EvertBunschoten <evertbun@xs4all.nl>
2025-04-30 10:55:01 +0200

    Set output folder to current working directory

--- RegressionTests/FluidGeneration/Air/generate_fluid_data.py
+++ RegressionTests/FluidGeneration/Air/generate_fluid_data.py
@@ -8,8 +8,9 @@ from su2dataminer.generate_data import DataGenerator_CoolProp
 
 Config = Config_NICFD(sys.argv[-1])
 
+Config.SetOutputDir(os.getcwd())
+
 D = DataGenerator_CoolProp(Config_in=Config)
-D.SetOutputDir(os.getcwd())
 
 D.PreprocessData()
 
