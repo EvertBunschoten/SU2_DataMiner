395bbee46c7339f6f633cd606fdfb610130f0683
EvertBunschoten <evertbun@xs4all.nl>
2025-05-20 14:23:52 +0200

    Added sphinx configuration files for documentation

---
+++ Documentation/source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+
+project = 'SU2 DataMiner'
+copyright = '2025, E.C.Bunschoten'
+author = 'E.C.Bunschoten'
+release = '2.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+add_module_names = False
+
+numpydoc_show_class_members = False
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ Documentation/source/index.rst
@@ -0,0 +1,33 @@
+.. SU2 DataMiner documentation master file, created by
+   sphinx-quickstart on Mon May 19 19:53:37 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+SU2 DataMiner
+=============
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+**flameletAI** (/lu'make/) is a Python library for cooks and food lovers that
+creates recipes mixing random ingredients.  It pulls data from the `Open Food
+Facts database <https://world.openfoodfacts.org/>`_ and offers a *simple* and
+*intuitive* API.
+
+.. note::
+
+   This project is under active development.
+.. toctree::
+   Config
+   :maxdepth: 2
+   :caption: Contents:
+   
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
